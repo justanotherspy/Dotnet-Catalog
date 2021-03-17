92f40d7d2e50d6aa7cf6446e33653f17bc03ec79
Daniel Schwartz <danielschwar@gmail.com>
2021-03-17 20:53:47 +0200

    Added PUT item to update functionality

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -26,7 +26,7 @@ namespace Catalog.Controllers
             return items;
         }
 
-        // GET /items/id
+        // GET /items/{id}
         [HttpGet("{id}")]
         public ActionResult<ItemDto> GetItem(Guid id)
         {
@@ -54,5 +54,20 @@ namespace Catalog.Controllers
             repository.CreateItem(item);
             return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
         }
+
+        // PUT /items/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+            if(existingItem is null) return NotFound();
+            Item updatedItem = existingItem with 
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+            repository.UpdateItem(updatedItem);
+            return NoContent();
+        } 
     }
 }
